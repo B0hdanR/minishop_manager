0fe9cdb9b6151bf5e56888823af4c9d2a9996b7b
Bohdan Romanov <bytovskyden@gmail.com>
2025-12-14 23:08:00 +0200

    add product categories and product models

--- shop/models.py
+++ shop/models.py
@@ -1,8 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class User(AbstractUser):
     is_employee = models.BooleanField(default=False)
 
-def __str__(self):
-    return self.username
+    def __str__(self):
+        return self.username
+
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    category = models.ForeignKey(
+        ProductCategory, on_delete=models.CASCADE, related_name="products"
+    )
+
+    def __str__(self):
+        return self.name
