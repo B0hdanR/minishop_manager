628ad82c60ae169298375a06a6d1ca716408d4a2
Bohdan Romanov <bytovskyden@gmail.com>
2025-12-22 21:43:17 +0200

    add product forms and delete confirmation template

---
+++ shop/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from shop.models import Product
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = [
+            "name",
+            "description",
+            "price",
+            "stock_quantity",
+            "category",
+        ]
