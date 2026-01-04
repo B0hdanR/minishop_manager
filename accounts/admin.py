5822e7462fa53859a1b8a437c4ddd31c3255fc0f
Bohdan Romanov <bytovskyden@gmail.com>
2026-01-05 00:08:03 +0200

    add spaces at the end of each file

--- accounts/admin.py
+++ accounts/admin.py
@@ -8,4 +8,3 @@ class SupportRequestAdmin(admin.ModelAdmin):
     list_display = ("id", "title", "content", "created_at", "is_processed", "user")
     list_filter = ("is_processed", "created_at")
     search_fields = ("title", "content", "user__username",)
-
